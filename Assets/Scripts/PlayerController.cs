5ead9c7e5a423a76d63041ca3404f3b7e8ac3551
Nikolai Henriksen <nikolhe@stud.ntnu.no>
2020-12-01 03:44:33 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D _rb2d;
+    bool _isJumping = false;
+    bool _isGrounded = true;
+    Vector2 _horizontalVelocity = new Vector2(0f, 0f);
+    float _jumpForce;
+
+    public void Move(Vector2 velocity) {
+        _horizontalVelocity = velocity;
+    }
+
+    public void Jump(float jumpForce) {
+        if (_isGrounded) {
+            _isJumping = true;
+            _jumpForce = jumpForce;
+        }
+    }
+
+    void Start() {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        _rb2d.MovePosition(_rb2d.position + _horizontalVelocity * Time.fixedDeltaTime);
+
+        if (_isJumping) {
+            _rb2d.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            Debug.Log("Jump => FixedUpdate");
+            Debug.Log(_rb2d.velocity);
+            _isJumping = false;
+        }
+        Debug.Log(_rb2d.velocity);
+
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        LayerMask ground = LayerMask.NameToLayer("Ground");
+
+        if (collision.gameObject.layer == ground) {
+            Debug.Log("Collision with ground");
+            _isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        LayerMask ground = LayerMask.NameToLayer("Ground");
+
+        if (collision.gameObject.layer == ground) {
+            Debug.Log("Collision exit with ground");
+            _isGrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerInput : MonoBehaviour {
+
+    public float movementSpeed = 5f;
+    public float jumpForce = 40f;
+
+    PlayerController controller;
+
+    void Start() {
+        controller = GetComponent<PlayerController>();
+    }
+
+    void Update() {
+        Vector2 moveVelocity = GetMovementInput();
+        controller.Move(moveVelocity);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            controller.Jump(jumpForce);
+        }
+    }
+
+    Vector2 GetMovementInput() {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        return moveInput.normalized * movementSpeed;
+    }
+}
